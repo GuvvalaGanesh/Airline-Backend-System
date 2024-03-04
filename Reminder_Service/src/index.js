b5affa53a12fb6a1c795a4542312dcbb2dd6a4cc
GuvvalaGanesh <ganeshguvvala00@gmail.com>
2024-03-04 10:32:59 +0530

    Setup SMTP using nodemailer done

--- Reminder_Service/src/index.js
+++ Reminder_Service/src/index.js
@@ -3,21 +3,23 @@ const bodyParser = require('body-parser');
 const app = express();
 
 const { PORT } = require('./config/serverConfig');
-const apiRoutes = require('./routes/index');
-const db = require('./models/index')
+const { sendBasicEmail } = require('./services/email-service');
 
 const setupAndStartServer = async () => {
 
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}));
 
-    app.use('/api', apiRoutes);
-
     app.listen(PORT, () => {
         console.log(`Server started on port ${PORT}`);
-        if(process.env.DB_SYNC) {
-            db.sequelize.sync({alter: true});
-        }
+
+        sendBasicEmail(
+                'support@admin.com',
+                'ganeshguvvala4@gmail.com',
+                'This is the testing email',
+                'Hiii, how are you?, I hope you like the service'
+        );
+
     })
 }
 
