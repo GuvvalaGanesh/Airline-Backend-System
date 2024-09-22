63370c81e69fd9d84c804ddfbb88d6159addb824
GuvvalaGanesh <ganeshguvvala00@gmail.com>
2024-09-22 16:38:19 +0530

    Remove nextjs folder

--- Auth_Service/my-nextjs-project/src/app/page.js
+++
@@ -1,9 +0,0 @@
-import Register from '../../pages/register';
-
-export default function Page() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-22">
-      <Register />
-    </main>
-  );
-}
